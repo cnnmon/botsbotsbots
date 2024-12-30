4444e91ecb63ada291a174f4c8e8e7282111488c
cnnmon <tiffanywang@berkeley.edu>
2024-12-30 14:53:49 -0500

    ganalytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import Desktop from '@/components/Desktop';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export default function Home() {
   return (
     <>
+      <GoogleAnalytics gaId="G-EB1C06ZDH2" />
       <Header />
       <Desktop />
       <Footer />
